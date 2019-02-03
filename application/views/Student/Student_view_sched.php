0cdb006d9b83bdc229660907bfc9f19247e29cc8
cristyfel <cristyfelpagutayao14@gmail.com>
2019-02-04 06:25:23 +0800

    k

--- application/views/Student/Student_view_sched.php
+++ application/views/Student/Student_view_sched.php
@@ -3,7 +3,7 @@ $this->load->library('session');
 $user_detail = $this->session->userdata('user_data_session');
 $level = $user_detail['type'];
 ?>
-<div id="wrapper">
+
 	<div id="page-wrapper"> 
 		 <div class="container-fluid">
            
@@ -16,132 +16,99 @@ $level = $user_detail['type'];
 						</li>
 					</ol>
 				</div>
-				</div>
-				<div class="month">      
-				  <ul>
-				    <li class="prev">&#10094;</li>
-				    <li class="next">&#10095;</li>
-				    <li>
-				      August<br>
-				      <span style="font-size:18px">2017</span>
-				    </li>
-				  </ul>
-				</div>
-
-				<ul class="weekdays">
-				  <li>Mo</li>
-				  <li>Tu</li>
-				  <li>We</li>
-				  <li>Th</li>
-				  <li>Fr</li>
-				  <li>Sa</li>
-				  <li>Su</li>
-				</ul>
-
-				<ul class="days">  
-				  <li>1</li>
-				  <li>2</li>
-				  <li>3</li>
-				  <li>4</li>
-				  <li>5</li>
-				  <li>6</li>
-				  <li>7</li>
-				  <li>8</li>
-				  <li>9</li>
-				  <li><span class="active">10</span></li>
-				  <li>11</li>
-				  <li>12</li>
-				  <li>13</li>
-				  <li>14</li>
-				  <li>15</li>
-				  <li>16</li>
-				  <li>17</li>
-				  <li>18</li>
-				  <li>19</li>
-				  <li>20</li>
-				  <li>21</li>
-				  <li>22</li>
-				  <li>23</li>
-				  <li>24</li>
-				  <li>25</li>
-				  <li>26</li>
-				  <li>27</li>
-				  <li>28</li>
-				  <li>29</li>
-				  <li>30</li>
-				  <li>31</li>
-				</ul>
 			</div>
-			<br>
 			<div class="row">
-				<table>
-				  <tr>
-				    <th>Form No</th>
-				    <th>Name of Event</th>
-				    <th>Department</th>
-				    <th>Date Reserved</th>
-				    <th>Time Reserved</th>
-				    <th>Date Actual Use</th>
-				    <th>Time Actual Use</th>
-				    <th>Purpose</th>
-				    <th>Equipment Needed</th>
-				    <th>Contact</th>
-				    <th>Status</th>
-				  </tr>
-				  <tr>
-				    <td>1</td>
-				    <td>DOST Seminar</td>
-				    <td>CITC</td>
-				    <td>October 25, 2018</td>
-				    <td>12:00</td>
-				    <td>November 1, 2018</td>
-				    <td>12:00</td>
-				    <td>Seminar</td>
-				    <td>Chair, Microphone</td>
-				    <td>09169947508</td>
-				    <td>Approved</td>
-				  </tr>
-				   <tr>
-				    <td>2</td>
-				    <td>Intramurals</td>
-				    <td>CSM</td>
-				    <td>November 25, 2018</td>
-				    <td>12:00</td>
-				    <td>November 1, 2018</td>
-				    <td>12:00</td>
-				    <td>Seminar, Aircon, Microphones</td>
-				    <td>Chair</td>
-				    <td>09169947508</td>
-				    <td>Approved</td>
-				  </tr>
-				 <!--  <tr>
-				   <td>Centro comercial Moctezuma</td>
-				   <td>Francisco Chang</td>
-				   <td>Mexico</td>
-				 </tr>
-				 <tr>
-				   <td>Ernst Handel</td>
-				   <td>Roland Mendel</td>
-				   <td>Austria</td>
-				 </tr>
-				 <tr>
-				   <td>Island Trading</td>
-				   <td>Helen Bennett</td>
-				   <td>UK</td>
-				 </tr>
-				 <tr>
-				   <td>Laughing Bacchus Winecellars</td>
-				   <td>Yoshi Tannamuri</td>
-				   <td>Canada</td>
-				 </tr>
-				 <tr>
-				   <td>Magazzini Alimentari Riuniti</td>
-				   <td>Giovanni Rovelli</td>
-				   <td>Italy</td>
-				 </tr> -->
-				</table>
-			</div>
+			    <div class="col-md-12">
+				
+			    <h1>Caelndar</h1>
+				<div id="calendar"></div>
+
+				<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Add Calendar Event</h4>
+				      </div>
+				      <div class="modal-body">
+				      <?php echo form_open(site_url("calendar/add_event"), array("class" => "form-horizontal")) ?>
+				      <div class="form-group">
+				                <label for="p-in" class="col-md-4 label-heading">Event Name</label>
+				                <div class="col-md-8 ui-front">
+				                    <input type="text" class="form-control" name="name" value="">
+				                </div>
+				        </div>
+				        <div class="form-group">
+				                <label for="p-in" class="col-md-4 label-heading">Description</label>
+				                <div class="col-md-8 ui-front">
+				                    <input type="text" class="form-control" name="description">
+				                </div>
+				        </div>
+				        <div class="form-group">
+				                <label for="p-in" class="col-md-4 label-heading">Start Date</label>
+				                <div class="col-md-8">
+				                    <input type="datetime-local" class="form-control" name="start_date">
+				                </div>
+				        </div>
+				        <div class="form-group">
+				                <label for="p-in" class="col-md-4 label-heading">End Date</label>
+				                <div class="col-md-8">
+				                    <input type="datetime-local" class="form-control" name="end_date">
+				                </div>
+				        </div>
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        <input type="submit" class="btn btn-primary" value="Add Event">
+				        <?php echo form_close() ?>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			    </div>
+		    </div>
+
+		    <div class="row">
+		    	<a data-toggle="modal" href="#addModal" class="btn btn-primary">Launch modal</a>
+					<div class="modal fade" id="myModal">
+					    <div class="modal-dialog">
+					      <div class="modal-content">
+					        <div class="modal-header">
+					          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+					          <h4 class="modal-title">Modal title</h4>
+					        </div>
+					        <div class="modal-body">
+					          Content for the dialog / modal goes here.
+					        </div>
+					        <div class="modal-footer">
+					          <a href="#" data-dismiss="modal" class="btn">Close</a>
+					          <a href="#" class="btn btn-primary">Save changes</a>
+					        </div>
+					      </div>
+					    </div>
+					</div>
+		    </div>
+
+		    <div id="calendarModal" class="modal fade">
+				<div class="modal-dialog">
+				    <div class="modal-content">
+				        <div class="modal-header">
+				            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span> <span class="sr-only">close</span></button>
+				            <h4 id="modalTitle" class="modal-title"></h4>
+				        </div>
+				        <div id="modalBody" class="modal-body"> </div>
+				        <div class="modal-footer">
+				            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				        </div>
+				    </div>
+				</div>
+				</div>
 		</div>
 	</div>
-</div>
+
+
+
+ 
+
+
 		
